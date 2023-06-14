aecccd84545c08384c79556078fb2afc5ba9a0a1
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-14 18:45:23 +0530

    [sanjana] Add : Practice_Problems

---
+++ Practice_Problem_3B/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Problem_3B
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           DemoExample demo = new DemoExample();
+            demo.Display();
+            DemoExample demo1 = new DemoExample(5,7);
+            demo1.Addition();
+
+        }
+    }
+     
+}
